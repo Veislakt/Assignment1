eb40182a445f2fde80cd067c4e667506e0c1b003
Veislakt <mhafredal@hotmail.com>
2017-02-14 16:37:53 +0100

    Added a class for Sensor objects for generating dummy values. Added functionality for the Sampling button and it is fully working.

--- WFAssignment1/WFAssignment1/Form1.Designer.cs
+++ WFAssignment1/WFAssignment1/Form1.Designer.cs
@@ -28,56 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnSampling = new System.Windows.Forms.Button();
+            this.btnLogging = new System.Windows.Forms.Button();
+            this.txtNextSampleTime = new System.Windows.Forms.TextBox();
+            this.txtNextLoggingTime = new System.Windows.Forms.TextBox();
+            this.txtSensorValues = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lblSampling = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSampling
             // 
-            this.button1.Location = new System.Drawing.Point(12, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Sampling";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSampling.Location = new System.Drawing.Point(12, 77);
+            this.btnSampling.Name = "btnSampling";
+            this.btnSampling.Size = new System.Drawing.Size(101, 23);
+            this.btnSampling.TabIndex = 0;
+            this.btnSampling.Text = "Sampling";
+            this.btnSampling.UseVisualStyleBackColor = true;
+            this.btnSampling.Click += new System.EventHandler(this.btnSampling_Click);
             // 
-            // button2
+            // btnLogging
             // 
-            this.button2.Location = new System.Drawing.Point(12, 180);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Logging on File";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLogging.Location = new System.Drawing.Point(12, 180);
+            this.btnLogging.Name = "btnLogging";
+            this.btnLogging.Size = new System.Drawing.Size(101, 23);
+            this.btnLogging.TabIndex = 1;
+            this.btnLogging.Text = "Logging on File";
+            this.btnLogging.UseVisualStyleBackColor = true;
+            this.btnLogging.Click += new System.EventHandler(this.btnLogging_Click);
             // 
-            // textBox1
+            // txtNextSampleTime
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 51);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtNextSampleTime.Location = new System.Drawing.Point(13, 51);
+            this.txtNextSampleTime.Name = "txtNextSampleTime";
+            this.txtNextSampleTime.Size = new System.Drawing.Size(100, 20);
+            this.txtNextSampleTime.TabIndex = 2;
             // 
-            // textBox2
+            // txtNextLoggingTime
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 154);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtNextLoggingTime.Location = new System.Drawing.Point(13, 154);
+            this.txtNextLoggingTime.Name = "txtNextLoggingTime";
+            this.txtNextLoggingTime.Size = new System.Drawing.Size(100, 20);
+            this.txtNextLoggingTime.TabIndex = 3;
             // 
-            // textBox3
+            // txtSensorValues
             // 
-            this.textBox3.Location = new System.Drawing.Point(159, 50);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 153);
-            this.textBox3.TabIndex = 4;
+            this.txtSensorValues.Location = new System.Drawing.Point(159, 50);
+            this.txtSensorValues.Multiline = true;
+            this.txtSensorValues.Name = "txtSensorValues";
+            this.txtSensorValues.Size = new System.Drawing.Size(100, 153);
+            this.txtSensorValues.TabIndex = 4;
             // 
             // label1
             // 
@@ -118,20 +121,30 @@
             this.lblSampling.Text = "Sampling";
             this.lblSampling.Click += new System.EventHandler(this.label4_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Sensor Values:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 224);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.lblSampling);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSensorValues);
+            this.Controls.Add(this.txtNextLoggingTime);
+            this.Controls.Add(this.txtNextSampleTime);
+            this.Controls.Add(this.btnLogging);
+            this.Controls.Add(this.btnSampling);
             this.Name = "Form1";
             this.Text = "DAQ Simulator";
             this.ResumeLayout(false);
@@ -141,15 +154,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnSampling;
+        private System.Windows.Forms.Button btnLogging;
+        private System.Windows.Forms.TextBox txtNextSampleTime;
+        private System.Windows.Forms.TextBox txtNextLoggingTime;
+        private System.Windows.Forms.TextBox txtSensorValues;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblSampling;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WFAssignment1/WFAssignment1/Form1.cs
+++ WFAssignment1/WFAssignment1/Form1.cs
@@ -12,9 +12,21 @@ namespace WFAssignment1
 {
     public partial class Form1 : Form
     {
+        //Initializing variables
+        Sensor sens1 = new Sensor(1);
+        Sensor sens2 = new Sensor(2);
+        Sensor sens3 = new Sensor(3);
+        Sensor sens4 = new Sensor(4);
+        DateTime nextSample = DateTime.Now;
+        DateTime nextLogging = DateTime.Now;
+        DateTime now;
         public Form1()
         {
             InitializeComponent();
+            txtNextSampleTime.Text = nextSample.ToString("HH:mm:ss.ff");
+            txtNextLoggingTime.Text = nextLogging.ToString("HH:mm:ss.ff");
+
+
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -28,6 +40,25 @@ namespace WFAssignment1
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSampling_Click(object sender, EventArgs e)
+        {
+            now = DateTime.Now;
+            if (now > nextSample)
+            {
+                txtSensorValues.Text = "Sensor 1:\r\n" + Convert.ToString(sens1.GetValue());
+                txtSensorValues.Text += "\r\nSensor 2:\r\n" + Convert.ToString(sens2.GetValue());
+                txtSensorValues.Text += "\r\nSensor 3:\r\n" + Convert.ToString(sens3.GetValue());
+                txtSensorValues.Text += "\r\nSensor 4:\r\n" + Convert.ToString(sens4.GetValue());
+                nextSample = now.AddMilliseconds(1400);
+                txtNextSampleTime.Text = nextSample.ToString("HH:mm:ss.ff");
+            }
+        }
+
+        private void btnLogging_Click(object sender, EventArgs e)
         {
 
         }
