ea3a1ab74728b1fe00d308581f853be0ca7d3707
Veislakt <mhafredal@hotmail.com>
2017-05-15 02:42:38 +0200

    Working on database classes

---
+++ AlarmApplication/AlarmApplication/DBAlarm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmApplication
+{
+    class DBAlarm
+    {
+        int AlarmId { get; set; }
+        DateTime ActivationTime { get; set; }
+        DateTime AcknowledgedTime { get; set; }
+        string AcknowledgedBy { get; set; }
+        string AlarmStatus { get; set; }
+        string Tag { get; set; }
+    }
+}
